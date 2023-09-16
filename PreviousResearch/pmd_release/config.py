a929e3f4233d356c6623e64ea2c68e55004aee43
IainHigh <iain.high@sky.com>
2023-09-16 13:02:55 +0100

    Title: Added "Progressive Mirror Detection" Code
    Description: Added the Progressive Mirror Detection code from Jiaying. Required minor modifications to get functional. Also added some directories and extensions to the gitignore to keep commit size down.
    
    Type:
    	[] Bug Fix
    	[] Refactoring
    	[x] New Feature
    
    Status:
    	[] Fully Functional
    	[] Needs Refactoring
    	[x] Features Missing
    	[] Tests Fail
    	[] Build Fails / Compilation Error

---
+++ PreviousResearch/pmd_release/config.py
@@ -0,0 +1,5 @@
+msd_testing_root = "/home/iain/Desktop/HonoursProject/Datasets/PMD_benchmark_combined/benchmark_NYUD/test"
+
+backbone_path = "./backbone/resnext/resnext_101_32x4d.pth"
+
+test_path = "./test_images/"
