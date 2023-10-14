b382e7307b1adfe5b6b2ca8ceffbe7787686cfb3
IainHigh <iain.high@sky.com>
2023-10-14 12:02:02 +0100

    Title: Modified HetNet previous research to get it functional
    Description:
    
    Type:
    	[x] Bug Fix
    	[] Refactoring
    	[] New Feature
    
    Status:
    	[] Fully Functional
    	[x] Needs Refactoring
    	[] Features Missing
    	[] Tests Fail
    	[] Build Fails / Compilation Error

--- PreviousResearch/HetNet/dataset.py
+++ PreviousResearch/HetNet/dataset.py
@@ -6,14 +6,15 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 
+
 ########################### Data Augmentation ###########################
 class Normalize(object):
     def __init__(self, mean, std):
-        self.mean = mean 
+        self.mean = mean
         self.std = std
-    
+
     def __call__(self, image, mask, edge=None):
-        image = (image - self.mean)/self.std
+        image = (image - self.mean) / self.std
         mask /= 255
         if edge is None:
             return image, mask
@@ -25,11 +26,11 @@ class Normalize(object):
 class RandomCrop(object):
     def __call__(self, image, mask, edge):
         H, W, _ = image.shape
-        randw = np.random.randint(W/8)
-        randh = np.random.randint(H/8)
+        randw = np.random.randint(W / 8)
+        randh = np.random.randint(H / 8)
         offseth = 0 if randh == 0 else np.random.randint(randh)
         offsetw = 0 if randw == 0 else np.random.randint(randw)
-        p0, p1, p2, p3 = offseth, H+offseth-randh, offsetw, W+offsetw-randw
+        p0, p1, p2, p3 = offseth, H + offseth - randh, offsetw, W + offsetw - randw
         return image[p0:p1, p2:p3, :], mask[p0:p1, p2:p3], edge[p0:p1, p2:p3]
 
 
@@ -47,9 +48,12 @@ class Resize(object):
         self.W = W
 
     def __call__(self, image, mask):
-        image = cv2.resize(image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
+        image = cv2.resize(
+            image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR
+        )
         mask = cv2.resize(mask, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
         return image, mask
+
 
 class ToTensor(object):
     def __call__(self, image, mask):
@@ -63,21 +67,21 @@ class ToTensor(object):
 class Config(object):
     def __init__(self, **kwargs):
         self.kwargs = kwargs
-        
-        if self.dataset == 'MSD':
+
+        if self.dataset == "MSD":
             # MSD
             self.mean = np.array([[[136.67972, 128.13225, 119.58932]]])
             self.std = np.array([[[64.26382, 67.363945, 68.53635]]])
-        elif self.dataset == 'PMD':
+        elif self.dataset == "PMD":
             # PMD
             self.mean = np.array([[[129.81274, 115.53708, 100.38846]]])
             self.std = np.array([[[65.67121, 65.61214, 67.4352]]])
         else:
             return None
-        
-        print('\nParameters...')
+
+        print("\nParameters...")
         for k, v in self.kwargs.items():
-            print('%-10s: %s' % (k, v))
+            print("%-10s: %s" % (k, v))
 
     def __getattr__(self, name):
         if name in self.kwargs:
@@ -95,19 +99,25 @@ class Data(Dataset):
         self.randomflip = RandomFlip()
         self.resize = Resize(352, 352)
         self.totensor = ToTensor()
-        with open(cfg.datapath+'/'+cfg.mode+'.txt', 'r') as lines:
+        with open(cfg.datapath + "/" + cfg.mode + ".txt", "r") as lines:
             self.samples = []
             for line in lines:
                 self.samples.append(line.strip())
 
     def __getitem__(self, idx):
         name = self.samples[idx]
-        image = cv2.imread(self.cfg.datapath+'/'+self.cfg.mode+'/image/'+name+'.jpg')[:,:,::-1].astype(np.float32)
-        mask = cv2.imread(self.cfg.datapath+'/'+self.cfg.mode+'/mask/'+name+'.png', 0).astype(np.float32)
+        image = cv2.imread(
+            self.cfg.datapath + "/" + self.cfg.mode + "/image/" + name + ".JPG"
+        )[:, :, ::-1].astype(np.float32)
+        mask = cv2.imread(
+            self.cfg.datapath + "/" + self.cfg.mode + "/mask/" + name + ".png", 0
+        ).astype(np.float32)
         shape = mask.shape
 
-        if self.cfg.mode == 'train':
-            edge = cv2.imread(self.cfg.datapath +'/'+self.cfg.mode+ '/edge/' + name + '.png', 0).astype(np.float32)
+        if self.cfg.mode == "train":
+            edge = cv2.imread(
+                self.cfg.datapath + "/" + self.cfg.mode + "/edge/" + name + ".png", 0
+            ).astype(np.float32)
             image, mask, edge = self.normalize(image, mask, edge)
             image, mask, edge = self.randomcrop(image, mask, edge)
             image, mask, edge = self.randomflip(image, mask, edge)
@@ -119,18 +129,24 @@ class Data(Dataset):
             return image, mask, shape, name
 
     def collate(self, batch):
-        if self.cfg.dataset == 'MSD':
-            size = [224, 256, 288, 320, 352][np.random.randint(0, 5)] # MSD
-        elif self.cfg.dataset == 'PMD':
-            size = [288, 320, 352, 384, 416][np.random.randint(0, 5)] # PMD
+        if self.cfg.dataset == "MSD":
+            size = [224, 256, 288, 320, 352][np.random.randint(0, 5)]  # MSD
+        elif self.cfg.dataset == "PMD":
+            size = [288, 320, 352, 384, 416][np.random.randint(0, 5)]  # PMD
         else:
-            return None    
-            
+            return None
+
         image, mask, edge = [list(item) for item in zip(*batch)]
         for i in range(len(batch)):
-            image[i] = cv2.resize(image[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR)
-            mask[i] = cv2.resize(mask[i],  dsize=(size, size), interpolation=cv2.INTER_LINEAR)
-            edge[i] = cv2.resize(edge[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR)
+            image[i] = cv2.resize(
+                image[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR
+            )
+            mask[i] = cv2.resize(
+                mask[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR
+            )
+            edge[i] = cv2.resize(
+                edge[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR
+            )
         image = torch.from_numpy(np.stack(image, axis=0)).permute(0, 3, 1, 2)
         mask = torch.from_numpy(np.stack(mask, axis=0)).unsqueeze(1)
         edge = torch.from_numpy(np.stack(edge, axis=0)).unsqueeze(1)
